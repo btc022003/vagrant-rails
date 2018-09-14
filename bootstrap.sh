95a709d50346e701a467096d075be1eeccf5473b
Arivin <btc022003@gmail.com>
2018-09-14 18:26:45 +0800

    init vagrant config

---
+++ bootstrap.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+sudo apt-get update
+sudo apt-get install -y libssl-dev libreadline-dev zlib1g-dev
+sudo apt-get install -y libsqlite3-dev
+
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+export NVM_DIR="/home/vagrant/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+nvm install node
+
+
+git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+export PATH="$HOME/.rbenv/bin:$PATH"
+export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"
+eval "$(rbenv init -)"
+rbenv install 2.5.1
+rbenv global 2.5.1
+gem install rails
+
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+
+sudo apt-get update && sudo apt-get install -y yarn
---
+++ readme.md
@@ -0,0 +1,10 @@
+#### Vagrant配置
+
+> rails5+nodejs开发环境配置文件,代码直接放在workspace中
+
+```bash
+vargant up # 安装虚拟机
+vargant ssh # 连接虚拟机
+# 映射本机3003端口到虚拟机3000端口
+# 可以自行查看安装后的环境
+```
