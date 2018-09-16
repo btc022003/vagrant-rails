cf89e65e8939c0e866085dc2e13f494bfd5869ba
Arivin <btc022003@gmail.com>
2018-09-16 09:39:23 +0800

    install build-essential for compiler

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 sudo apt-get update
-sudo apt-get install -y libssl-dev libreadline-dev zlib1g-dev
+sudo apt-get install -y libssl-dev libreadline-dev zlib1g-dev build-essential
 sudo apt-get install -y libsqlite3-dev
 
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
